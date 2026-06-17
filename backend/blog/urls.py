f48be245fde5a1726d62bc21934623381c012341
Wilson Kilonzo <mutidawilz@gmail.com>
2026-06-17 07:37:03 +0300

    Temporarily remove sitemap to fix import error

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,9 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import BlogPostViewSet, sitemap
+from .views import BlogPostViewSet
 from django.urls import path
 
 router = DefaultRouter()
 router.register(r'blog', BlogPostViewSet, basename='blog')
 urlpatterns = [
-    path('sitemap.xml', sitemap, name='sitemap'),
+    # path('sitemap.xml', sitemap, name='sitemap'),
 ] + router.urls
